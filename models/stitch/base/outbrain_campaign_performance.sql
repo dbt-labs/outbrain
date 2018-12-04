ce47dc0b99f02f8d481b6f3c4281ac9ac6294356
Alexa Diaz <axela.zaid@gmail.com>
2018-12-04 16:23:09 -0500

    added packages file; referenced dbt utils in models when necessary

--- models/stitch/base/outbrain_campaign_performance.sql
+++ models/stitch/base/outbrain_campaign_performance.sql
@@ -1,7 +1,6 @@
 select
 
-    md5(fromdate || campaignid) as id,
-
+    {{ dbt_utils.surrogate_key('fromdate', 'campaignid') }} as id,
     to_date(fromdate, 'yyyy-mm-dd') as date_day,
     campaignid as campaign_id,
 
--- models/stitch/base/outbrain_campaigns.sql
+++ models/stitch/base/outbrain_campaigns.sql
@@ -8,12 +8,12 @@ select
     livestatus__onairreason as status,
     currency,
     targeting__language as targeting_language,
-
-    split_part(split_part(suffixtrackingcode,'utm_source=',2), '&', 1) as utm_source,
-    split_part(split_part(suffixtrackingcode,'utm_medium=',2), '&', 1) as utm_medium,
-    split_part(split_part(suffixtrackingcode,'utm_campaign=',2), '&', 1) as utm_campaign,
-    split_part(split_part(suffixtrackingcode,'utm_content=',2), '&', 1) as utm_content,
-    split_part(split_part(suffixtrackingcode,'utm_term=',2), '&', 1) as utm_term,
+    
+    {{ dbt_utils.get_url_parameter('suffixtrackingcode', 'utm_source') }} as utm_source,
+    {{ dbt_utils.get_url_parameter('suffixtrackingcode', 'utm_medium') }} as utm_medium,
+    {{ dbt_utils.get_url_parameter('suffixtrackingcode', 'utm_campaign') }} as utm_campaign,
+    {{ dbt_utils.get_url_parameter('suffixtrackingcode', 'utm_content') }} as utm_content,
+    {{ dbt_utils.get_url_parameter('suffixtrackingcode', 'utm_term') }} as utm_term,
 
     enabled,
     creationtime created_at,
